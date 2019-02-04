4d56893669ca942d0723f74174e3a6b73cb17bc4
Ryan Do <ryan.do@mail.utoronto.ca>
2019-02-04 17:39:45 -0500

    implemented 3.1.2 (SGD), needs testing

--- a1/starter.py
+++ a1/starter.py
@@ -48,7 +48,7 @@ W = np.zeros((28, 28))
 b = 0
 lrs = [0.005, 0.001, 0.0001]
 error_tolerance = 1e-7
-epochs = 5000
+epochs = 500
 reg = [0, 0.001, 0.1, 0.5]
 
 
@@ -149,7 +149,6 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
 
 
 
-
     for epoch in range(iterations):
         #one pass through entire dataset
 
@@ -226,8 +225,6 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
 
 
 
-
-
 def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=0.001):
 
     # Initialize weight and bias tensors
@@ -240,23 +237,145 @@ def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rat
 
 
     tf.set_random_seed(421)
-    if loss == "MSE":
+    if lossType == "MSE":
         loss_tensor = MSE(W=weights, b=bias, x=data, y=labels, reg=regularization)
         predicted_labels = tf.matmul(trainingData, weights) + b
-    elif loss == "CE":
+    elif lossType == "CE":
         loss_tensor = crossEntropyLoss(W=weights, b=bias, x=data, y=labels, reg=regularization)
         predicted_labels = tf.sigmoid(tf.matmul(trainingData, weights))
 
     opt = GradientDescentOptimizer(learning_rate=learning_rate)
     opt_op = opt.minimize(loss_tensor, var_list=[W, b])
 
-    return weights, bias, predicted_labels, labels, loss_tensor, opt_op, regularization
+    return weights, bias, data, predicted_labels, labels, loss_tensor, opt_op, regularization
+
+def SGD(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS, minibatch_size, lossType = None):
+    '''
+
+    :param W: weight matrix
+    :param b: bias vector
+    :param trainingData: (3500, 28, 28)
+    :param trainingLabels: (3500, 1)
+    :param alpha:
+    :param iterations: num epochs
+    :param reg: regularization constant
+    :param EPS: error tolerance
+    :return: optimized weight and bias vectors
+    '''
+
+    start_time = time.time()
+    #Initialize weight matrix with random
+
+    #weights, bias, data, predicted_labels, labels, loss_tensor, opt_op, regularization = buildGraph(lossType="MSE")
+
+    trainingData = np.reshape(trainingData, (trainingData.shape[0], -1))
+    W = np.reshape(W, (W.shape[0] * W.shape[1], -1)) #flatten 2nd weight matrix
+    N = trainingLabels.shape[0]
+
+    for epoch in range(iterations):
+        #one pass through entire dataset
+
+        trainbatches = sample_batches(trainingData, trainingLabels, minibatch_size)
+
+        for trainbatch in enumerate(trainbatches):
+            print("Epoch: {}, Batch: {}".format(epoch+1, trainbatch[0]))
+            batch_data = trainbatch[1][:,0:-1]
+            batch_labels = np.expand_dims(trainbatch[1][:,-1], axis=1)
+
+            if lossType == "MSE":
+                gradients = gradMSE(W, b, batch_data, batch_labels, reg) #trainbatch[:,0:-2], trainbatch[:,-1] gives data and label for each batch
+
+                train_loss = MSE(W, b, batch_data, batch_labels, reg)
+                val_loss = MSE(W, b, validData, validTarget, reg)
+                test_loss = MSE(W, b, testData, testTarget, reg)
+
+            elif lossType == "CE":
+                gradients = gradCE(W, b, batch_data, batch_labels, reg)
+
+                train_loss = crossEntropyLoss(W, b, batch_data, batch_labels, reg)
+                val_loss = crossEntropyLoss(W, b, validData, validTarget, reg)
+                test_loss = crossEntropyLoss(W, b, testData, testTarget, reg)
+
+
+            grad_weights = gradients[0]
+            grad_biases = gradients[1]
+
+            W = W - alpha*grad_weights #(784x1) update weights
+            b = b - alpha*grad_biases #(784x1) update bias
+
+        ###### CALCULATE ACCURACIES AND STORE/PRINT AT EACH EPOCH #######################################################
+        # based on entire training set (unbatched)
+        if lossType == "MSE":
+
+            predicted_train = np.matmul(trainingData, W) + b
+            predicted_val = np.matmul(validData, W) + b
+            predicted_test = np.matmul(testData, W) + b
+
+            predicted_train[predicted_train > 0] = 1
+            predicted_train[predicted_train < 0] = 0
+
+            predicted_val[predicted_val > 0] = 1
+            predicted_val[predicted_val < 0] = 0
+
+            predicted_test[predicted_test > 0] = 1
+            predicted_test[predicted_test < 0] = 0
+
+        elif lossType == "CE":
+
+            test = logistic_y_hat(W, trainingData, b)
+            predicted_train = np.expand_dims(np.array([1 if i > 0.5 else 0 for i in logistic_y_hat(W, trainingData, b)]), axis=1)
+            predicted_val = np.expand_dims(np.array([1 if i > 0.5 else 0 for i in logistic_y_hat(W, validData, b)]), axis=1)
+            predicted_test = np.expand_dims(np.array([1 if i > 0.5 else 0 for i in logistic_y_hat(W, testData, b)]), axis=1)
+
+
+        train_acc = np.sum(predicted_train == trainingLabels) / N
+        val_acc = np.sum(predicted_val == validTarget) / validTarget.shape[0]
+        test_acc = np.sum(predicted_test == testTarget) / testTarget.shape[0]
+
+        #storing losses and accuracies
+        trainloss_list.append(train_loss)
+        valloss_list.append(val_loss)
+        testloss_list.append(test_loss)
+
+        train_acc_list.append(train_acc)
+        val_acc_list.append(val_acc)
+        test_acc_list.append(test_acc)
+
+
+        print("Epoch: {}, | Training loss: {:.5f}  | Validation Loss: {:.5f} |  Test Loss: {:.5f} | "
+              "Training Accuracy: {:.5f}  | Validation Accuracy: {:.5f} |  Test Accuracy: {:.5f}"
+              .format(epoch + 1, train_loss, val_loss, test_loss, train_acc, val_acc, test_acc))
+
+        if np.linalg.norm(grad_weights) <= EPS or np.linalg.norm(grad_biases) <= EPS:
+           pass
+        #############################################################################################################
+
+    elapsed_time = int(time.time() - start_time)
+
+    print("Training Complete, Time taken is: {:02d}:{:02d}:{:02d}".format(elapsed_time // 3600, (elapsed_time % 3600 // 60), elapsed_time % 60))
+
+    return W, b
+
+def sample_batches(trainingData, trainingLabels, minibatch_size):
+    trainbatch = np.concatenate((trainingData, trainingLabels), axis=1)  # label is last element of each row (trainbatch[:,785-1]
+
+    np.random.shuffle(trainbatch)
+
+    trainbatches = np.split(trainbatch, trainbatch.shape[0] // minibatch_size, axis=0)  # subset shuffled datasets
+
+    return trainbatches
+
 
 test_normal = False
-test_GD = True
+test_GD = False
+test_SGD = True
 
 if test_GD:
     W, b = grad_descent(W, b, trainData, trainTarget, lrs[0], epochs, reg[0], error_tolerance, 'CE')
+    plot(epochs, trainloss_list, valloss_list, testloss_list, train_acc_list, val_acc_list, test_acc_list, True)
+
+if test_SGD:
+    W, b = SGD(W, b, trainData, trainTarget, lrs[0], epochs, reg[0], error_tolerance, 500, 'CE')
     plot(epochs, trainloss_list, valloss_list, testloss_list, train_acc_list, val_acc_list, test_acc_list, True)
 
 if test_normal:
