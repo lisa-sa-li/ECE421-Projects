54ad8e55be4ff7185acd9ad3172a44f3caa7864f
Exquisition <l.zhou@mail.utoronto.ca>
2019-03-10 18:34:11 -0400

    plotting function reused from A1

---
+++ a2/plotting.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot(epochs, train_loss, val_loss, test_loss, train_acc, val_acc, test_acc):
+
+    x = [i for i in range(1, epochs + 1)]
+
+    plt.plot(x, train_loss, label="Training loss")
+    plt.plot(x, val_loss, label="Validation loss")
+    plt.plot(x, test_loss, label="Test loss")
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+
+    plt.legend()
+    plt.title('Loss Curves')
+    plt.show()
+
+    plt.plot(x, train_acc, label="Training Accuracy")
+    plt.plot(x, val_acc, label="Validation Accuracy")
+    plt.plot(x, test_acc, label="Test Accuracy")
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+
+    plt.legend()
+    plt.title('Accuracy Curves')
+
+    plt.show()
+
+
+
